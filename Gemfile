d8ec1ae5eb675799da1602a73fe9300a2dffb511
jacklin0501 <jack200994798@163.com>
2020-09-08 23:25:08 +0800

    Server static assets

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '2.13.1'
+  gem 'sqlite3', '~> 1.4'
 end
 
 group :development do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+Sampleapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+config.server_static_assets = true
 end
